5aa9b7f5e37c870f007f91e3e3111a1c175eba17
KTran-Tech <kevinmt191919@gmail.com>
2020-05-12 14:38:28 -0500

    more files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const autoprefixer = require('autoprefixer')
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
@@ -8,5 +8,31 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: ''
     },
-    devtool: 'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map',
+    module:{
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test:/\.css$/,
+                exclude: /node_modules/,
+                use:[
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader', options:{
+                        importLoaders: 1,
+                        modules:{
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                    }},
+                    {loader: 'postcss-loader', options:{
+                        ident: 'postcss',
+                        plugins:()=>[autoprefixer()]
+                    }}
+                ]
+            }
+        ]
+    }
 };
